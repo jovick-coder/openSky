35f523ac5bf2dfe2aa7138c6d657c6840272493c
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-09 12:38:32 +0000

    clean space

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App .css";
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
 import Login from "./pages/LoginPage/Login";
