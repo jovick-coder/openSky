3e5e99515961cb309c3bb0c9fcd0f6684161f07d
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-11 10:26:25 +0000

    added an overlay added link to the button

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import desktopHeroImage from "../../assets/images/airplan4.jpg";
+import { Link } from "react-router-dom";
+import desktopHeroImage from "../../assets/images/airplan1.jpg";
 import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
     <>
       <div className="HeroSection">
+        <div className="overlay"></div>
         <div className=" img-div">
           <img src={desktopHeroImage} alt="airplane" className="bg-image" />
           <div className="text-div">
             {" "}
-            <h1 className="ms-4">Get Flight update </h1>
+            <h1 className="ms-4">Get live Flight update </h1>
             <div className="button-div">
-              <button>Get Started</button>
+              <Link to="/login">
+                <button>Get Started</button>
+              </Link>
             </div>
           </div>
         </div>
