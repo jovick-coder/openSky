a6c9d048fba69f3e60ba81b5e907c5c223a252fa
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-11 10:25:22 +0000

    created a component for footer

---
+++ src/components/Footer/FooterComponent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function FooterComponent() {
+  return (
+    <div className="footer text-center py-3">
+      © 2013 - 2022 The OpenSky Network |{" "}
+      <a href="https://opensky-network.org/index.php/about/terms-of-use">
+        Terms of Use
+      </a>{" "}
+      |{" "}
+      <a href="https://opensky-network.org/index.php/about/privacy-policy">
+        Privacy Policy
+      </a>{" "}
+      | <a href="https://opensky-network.org/index.php/about/faq"> FAQ</a> |
+      <a href="https://opensky-network.org/index.php/about/contact">Contact</a>{" "}
+    </div>
+  );
+}
+
+export default FooterComponent;
